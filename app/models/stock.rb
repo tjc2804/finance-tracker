624940384c5a8f77d5a8acd4d002dd0fe39dc86b
jichong <tay_ji_chong@hotmail.com>
2023-06-18 12:37:37 +0800

    ajax form

---
+++ app/models/stock.rb
@@ -0,0 +1,14 @@
+class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:publishable_key],
+                                  secret_token: Rails.application.credentials.iex_client[:secret_key],
+                                  endpoint: 'https://cloud.iexapis.com/v1')
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+    end
+  end
+
+end
